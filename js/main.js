0108a9a5686fab71cd0cb7a93f9fbaf99091f6f4
Siddharth Girkar <girkarsiddharth@gmail.com>
2017-08-17 20:03:55 -0700

    Changing it up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("Good morning, USA!");
+$('.navbar-collapse ul li a').click(function(){
+    $('.navbar-toggle:visible').click();
+});
+
+$('.navbar-brand').click(function(){
+    $('.navbar-toggle:visible').click();
+});
+
+$(".flip").flip();
+
+$("#websiteTitle").typeIt({
+    strings: [" Siddharth.", " Sid."],
+    speed: 100,
+    breakLines: false,
+    autoStart: false,
+    loop: false,
+    loopDelay: 2000,
+    deleteSpeed: 100
+});
+
+var scroll = new SmoothScroll('a[href*="#"]');
